09029c04975b527dd8409105c506ba526b79141f
igor fagundes pontes <128741930+igorfagundes@users.noreply.github.com>
2023-07-23 12:03:30 -0300

    adicionando spring initialz
    adicionando pastas do spring initialz

---
+++ src/main/java/com/thecode/controlededados/ControlededadosApplication.java
@@ -0,0 +1,13 @@
+package com.thecode.controlededados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlededadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlededadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thecode/controlededados/ControlededadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thecode.controlededados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlededadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
